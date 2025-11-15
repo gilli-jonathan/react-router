f75ad3eff63584888c2615d170277bc06dcb7c57
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-11-15 20:50:42 +0100

    create basis route in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" Component={HomePage} />
+            <Route path="/chi-siamo" element={<AboutPage />} />
+            <Route path="/prodotti" element={<ContactsPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
 
     </>
   )
