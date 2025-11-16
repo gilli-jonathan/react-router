e5d48633efc29c18ae32fdf818ec3a2435650286
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-11-16 20:54:42 +0100

    add unbo with bootstrap

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,48 @@ export default function HomePage() {
 
     return (
         <>
+            <div className="row align-items-md-stretch">
+                <div className="col-md-6">
+                    <div
+                        className="h-100 p-5 text-white bg-primary border rounded-3"
+                    >
+                        <h2>Change the background</h2>
+                        <p>
+                            Swap the background-color utility and add a `.text-*` color
+                            utility to mix up the jumbotron look. Then, mix and match with
+                            additional component themes and more.
+                        </p>
+                        <button
+                            className="btn btn-outline-primary"
+                            type="button"
+                        >
+                            Example button
+                        </button>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div
+                        className="h-100 p-5 bg-primary border rounded-3"
+                    >
+                        <h2>Add borders</h2>
+                        <p>
+                            Or, keep it light and add a border for some added definition
+                            to the boundaries of your content. Be sure to look under the
+                            hood at the source HTML here as we've adjusted the alignment and
+                            sizing of both column's content for equal-height.
+                        </p>
+                        <button
+                            className="btn Swap the background-color utility and add a `.text-*` color
+                        utility to mix up the jumbotron look. Then, mix and match with
+                        additional component themes and more."
+                            type="button"
+                        >
+                            Example button
+                        </button>
+                    </div>
+                </div>
+            </div>
+
 
 
 
