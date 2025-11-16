bbe6e17de26692085234dea6b415d5d224468d5f
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-11-16 20:53:15 +0100

    add pic

--- src/pages/ChiSiamoPage.jsx
+++ src/pages/ChiSiamoPage.jsx
@@ -2,8 +2,46 @@ export default function ChiSiamoPage() {
 
 
     return (
-        <>
-        </>
+        <main>
+            <div id="banner-black" >
+
+                <img src="https://www.economyup.it/wp-content/uploads/2025/06/01_Team-Wibo-Sales-1620x1080.jpg" alt="" />
+
+            </div>
+
+            <section id="us-p">
+                <h3 className="py-3">CHI SIAMO?</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum blanditiis atque, hic nulla sequi eos dolores nemo voluptate quisquam odio. Ut, praesentium beatae alias nesciunt totam iusto similique voluptatibus consectetur!
+                    Assumenda minus quam consequatur distinctio culpa unde quia, officia magnam, dolorum ea fugit cumque repellendus aliquam ut accusamus commodi sit cupiditate perferendis totam natus ipsum odio? Minus, asperiores enim. A?
+                    Iure ducimus est et nostrum quisquam, nisi repellendus doloribus numquam autem, libero dolor dolorum veniam odio ipsum ab laboriosam porro reiciendis fugit explicabo alias! Aliquam dolorum nobis incidunt ex error?
+                    Sint cum minima esse eveniet dolores adipisci! Accusamus omnis iure dolore. Ipsam expedita quis, eveniet delectus illum aspernatur autem cumque mollitia qui voluptas incidunt ea voluptatibus nam doloribus a alias.
+                    Repellendus officiis incidunt consequuntur placeat distinctio, ipsum ad eos explicabo! Quos, exercitationem quidem! Atque ullam quam necessitatibus veniam temporibus ipsam fugit earum iure, perferendis laudantium quis aliquam harum eos iusto.
+                    Hic expedita dolorem amet. Culpa dolorum asperiores dignissimos quaerat dolor, blanditiis tenetur deleniti. Tempore veritatis eligendi corrupti explicabo alias, nostrum doloribus harum perferendis temporibus enim earum porro est molestiae quo.
+                    Reprehenderit deserunt qui officiis eveniet vitae ad rerum explicabo nulla, exercitationem labore est consequuntur fugit modi tempore consequatur amet non? Incidunt suscipit soluta velit, vel enim rem facilis. Excepturi, obcaecati!
+                    Alias amet recusandae fugit soluta, cum nesciunt at id non eligendi, temporibus unde quidem sed eum, accusantium accusamus numquam quae quo commodi praesentium odit voluptatibus animi suscipit rerum! Facilis, quos.
+                    Voluptatibus deleniti omnis, ipsam enim eaque perferendis neque, dicta totam dolores excepturi doloribus illo, quia incidunt officiis quisquam in! Voluptatem ratione sapiente facere earum id! Nemo quod ducimus animi fugiat.
+                    Perferendis recusandae reprehenderit corporis adipisci fugit doloremque optio veritatis non accusantium dolor aperiam vitae eius et dicta eligendi quaerat tempore, natus maxime alias error. Iste et perferendis fugiat explicabo impedit!
+                    Accusamus, vero cupiditate temporibus itaque maiores quam possimus porro libero doloremque iste omnis fugiat doloribus vel fuga sit dolores incidunt assumenda, mollitia optio iure quo. Officiis temporibus deserunt natus! Itaque!
+                    Eum vitae tempora blanditiis, eaque minima repellendus sit debitis ut esse non veritatis sapiente amet suscipit, fuga natus nemo odit minus incidunt, placeat sed delectus quidem voluptates? Deleniti, ipsam nesciunt!
+                    Nemo dolorem voluptate voluptates voluptas molestias reiciendis culpa magni expedita accusantium, sint eveniet quae iusto. Officia dolores error, doloribus quasi, iure, consectetur cumque ex exercitationem explicabo qui rem optio ipsum.
+                    Accusamus autem officia quasi odit dolor velit saepe incidunt perspiciatis, exercitationem eius nostrum nihil harum maiores. Vero dolorum beatae optio earum laborum, molestias voluptate officiis, impedit ullam deleniti ipsa maiores.
+                    Laborum culpa aspernatur quis consequatur totam blanditiis. Ipsam quasi provident expedita commodi excepturi atque ab consectetur dolorum ad eius vitae nemo amet dolorem, sed ex ipsum, facilis nobis pariatur voluptatum.
+                    Ullam quam ad inventore in? Quasi autem officiis vitae quae dolorum ullam aut magnam. Maxime dolore atque ipsam velit repudiandae sunt et dignissimos. Atque error animi eum debitis assumenda consequatur!
+                    Distinctio ratione beatae veritatis harum quibusdam obcaecati tenetur totam commodi repudiandae iusto, accusamus architecto iste, nulla, quos dolore consequatur minima et? Eligendi deleniti ullam rem adipisci nam qui aut sed?
+                    Magni asperiores officia eos natus molestias hic eius doloremque in, laboriosam rerum ullam, quod ratione architecto commodi id debitis et! Aspernatur rerum molestiae error quae, a dolorem amet tempore reprehenderit.
+                    Voluptate, vitae nostrum perspiciatis harum sunt accusantium vel aperiam, quae quidem sit maxime obcaecati laborum consequuntur odio amet explicabo enim fugit culpa nisi. Natus porro ea voluptate quis, delectus voluptatem.
+                    Quae quibusdam quisquam quidem harum error ipsam, blanditiis rem ipsa! Sed aspernatur, repellat, et vel alias necessitatibus accusamus illo amet vitae deserunt eligendi, id quod quae voluptate nam illum enim!
+                    Tenetur non molestias vero iste eligendi quibusdam nemo reiciendis distinctio amet nam libero exercitationem, incidunt sunt. Incidunt, voluptas facere repellendus asperiores accusamus, maiores laudantium ut tempore quam sed exercitationem dolore.
+                    Provident illum numquam, voluptate error maiores suscipit, veniam ullam, libero qui illo asperiores consequuntur dolorem. Molestiae, officia culpa in porro expedita quod perferendis illum unde adipisci quis sint necessitatibus corrupti.
+                    Similique, suscipit! Dignissimos dolores maiores vero amet ducimus, consequatur perferendis nulla aperiam veniam rem sint ullam fugit maxime. Magni obcaecati nisi nesciunt quidem beatae quos quo deleniti ad incidunt nobis.
+                    Facere vero molestiae doloribus ea incidunt et laudantium facilis, blanditiis tempora, porro sequi alias neque eum. Asperiores nisi recusandae ad a quod maxime dolorum consequatur magnam, provident ex illum laudantium.
+                </p>
+            </section>
+
+
+
+
+        </main>
 
 
     )
