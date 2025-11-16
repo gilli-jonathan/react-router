b666c0ea5cdbde9ec5e912878683b9b8a171ac61
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-11-16 17:36:09 +0100

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,10 @@ export default function Footer() {
 
 
     return (
-        <footer>
-
+        <footer class="py-5 bg-dark">
+            <div class="container">
+                <p class="m-0 text-center text-white">Jon & Jon &copy;</p>
+            </div>
         </footer>
     )
 }
