e40377c87b4f169472f3584f4a2e5c2cbbdcdaf5
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-11-16 08:44:00 +0100

    dinamic nav manu in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
 
@@ -18,23 +18,25 @@ export default function Header() {
                     <Link className="navbar-brand" to="/">ICON</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                            <li className="nav-item"><a className="nav-link active" aria-current="page" href="#!">Home</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#!">About</a></li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a className="dropdown-item" href="#!">All Products</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="#!">Popular Items</a></li>
-                                    <li><a className="dropdown-item" href="#!">New Arrivals</a></li>
-                                </ul>
-                            </li>
+                            {
+                                nav_header.map((obj) => (
+
+                                    <li key={obj.id} className="nav-item">
+                                        <NavLink className="nav-link" aria-current="page" to={obj.link}>{obj.title}</NavLink>
+                                    </li>
+
+                                ))
+                            }
+
                         </ul>
+
+
                         <form className="d-flex">
                             <button className="btn btn-outline-dark" type="submit">
                                 <i className="bi-cart-fill me-1"></i>
-                                Cart
+                                Carrello
                                 <span className="badge bg-dark text-white ms-1 rounded-pill">0</span>
                             </button>
                         </form>
