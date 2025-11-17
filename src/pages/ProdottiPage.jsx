276a4b24f475e2a98d3c106ee8c9437f422ebf37
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-11-17 12:46:48 +0100

    change button icon with an simple heart

--- src/pages/ProdottiPage.jsx
+++ src/pages/ProdottiPage.jsx
@@ -60,7 +60,7 @@ export default function ProdottiPage(arr) {
 
                                             <a class="btn btn-outline-dark mt-auto m-2" href="#">info</a>
                                             <a class="btn btn-outline-success mt-auto m-2" href="#"><i class="bi bi-bag-plus-fill"></i></a>
-                                            <a class="btn btn-outline-danger mt-auto m-2" href="#"><i class="bi bi-bag-heart-fill"></i></a>
+                                            <a class="btn btn-outline-danger mt-auto m-2" href="#"><i class="bi bi-heart-fill"></i></a>
 
                                         </div>
                                     </div>
